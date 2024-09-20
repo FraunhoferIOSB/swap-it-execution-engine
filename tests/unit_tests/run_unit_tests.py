9b61683ce49e2e69f99b35b8c5cd2aeab72adfed
FlorianDue <florian.duewel@iosb.fraunhofer.de>
2024-09-20 12:22:48 +0200

    feat(tests): correct file integration

--- tests/unit_tests/run_unit_tests.py
+++ tests/unit_tests/run_unit_tests.py
@@ -4,11 +4,11 @@
 
 # Copyright 2023-2024 (c) Fraunhofer IOSB (Author: Florian Düwel)
 import sys, os, unittest
-sys.path.append(os.path.join(os.path.dirname(__file__), "..\\..\\."))
-sys.path.append(os.path.join(os.path.dirname(__file__), "..\\..\\tests\\test_helpers\\."))
-sys.path.append(os.path.join(os.path.dirname(__file__), "..\\..\\control_interface\\."))
-sys.path.append(os.path.join(os.path.dirname(__file__), "..\\..\\execution_engine_logic\\."))
-sys.path.append(os.path.join(os.path.dirname(__file__), "..\\..\\dispatcher\\."))
+sys.path.append(os.path.join(os.path.dirname(__file__), "..//..//."))
+sys.path.append(os.path.join(os.path.dirname(__file__), "..//..//tests/test_helpers//."))
+sys.path.append(os.path.join(os.path.dirname(__file__), "..//..//control_interface//."))
+sys.path.append(os.path.join(os.path.dirname(__file__), "..//..//execution_engine_logic//."))
+sys.path.append(os.path.join(os.path.dirname(__file__), "..//..//dispatcher//."))
 from check_control_interface.run_unit_tests import RunControlInterfaceTests
 from check_execution_engine_logic.run_unit_tests import RunExecutionEngineLogicTests
 from check_dispatcher.run_unit_tests import RunDispatcherTests
