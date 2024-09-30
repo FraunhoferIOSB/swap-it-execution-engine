8747fade627a8a13c14aee6f3f8c3485e3a36f5f
florian.duewel@iosb.fraunhofer.de <flo@KIS-FLO47663-2.pc.iitb.fhg.de>
2024-09-30 16:42:05 +0200

    feat(ExecutionEngine): refactor code

--- execution_engine_logic/execution_engine.py
+++ execution_engine_logic/execution_engine.py
@@ -7,7 +7,6 @@
 import asyncio
 from datetime import datetime
 from asyncua import ua
-from multiprocessing import Process
 from execution_engine_logic.data_types.internal_data_converter import EngineOpcUaDataConverter
 from execution_engine_logic.execution_engine_server import ExecutionEngineServer
 from execution_engine_logic.data_object.data_object_interaction import DataObject
