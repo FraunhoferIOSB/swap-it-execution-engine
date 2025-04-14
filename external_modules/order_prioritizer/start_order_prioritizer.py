7276e4aa034923c55034a71dbad5ac0328dd4043
flo47663 <florian.duewel@iosb.fraunhofer.de>
2025-04-14 10:14:12 +0200

    feat(ExternalModules): correct year in copy right

--- external_modules/order_prioritizer/start_order_prioritizer.py
+++ external_modules/order_prioritizer/start_order_prioritizer.py
@@ -2,10 +2,9 @@
 # For details on the licensing terms, see the LICENSE file.
 # SPDX-License-Identifier: MIT
 
-# Copyright 2023-2024 (c) Fraunhofer IOSB (Author: Florian Düwel)
+# Copyright 2025 (c) Fraunhofer IOSB (Author: Florian Düwel)
 import asyncio
 from external_modules.order_prioritizer.server.run_prioritizer_instance import PrioritizerServer
-
 
 if __name__ == "__main__":
     asyncio.run(PrioritizerServer("opc.tcp://localhost:", "12000", "opc.tcp://localhost:8000", "opc.tcp://localhost:", 5).run_server())
